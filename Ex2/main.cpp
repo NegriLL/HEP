5d7890d18a0a2238ad04acb7b3e75649b020eb9d
NegriLL <leonardo.negri@helsinki.fi>
2023-01-27 18:45:44 +0200

    complete first part of ex2

---
+++ Ex2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    cout << "Write an integer and press enter: ";
+    cin >> number;
+    cout << "Hello World " << number<< endl;
+    return 0;
+}
