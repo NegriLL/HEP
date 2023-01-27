7e6053f26505898dc98e1caedbefb42314c8f94b
NegriLL <leonardo.negri@helsinki.fi>
2023-01-27 22:43:15 +0200

    done exercise 2

---
+++ Ex2/part2.py
@@ -0,0 +1,17 @@
+import re
+import sys
+
+# I found it easier to use exact text matching rather than regex for this exercise
+
+path = r"./Ex2/brilcalc.log"
+
+with open(path) as f:
+    while f.readline() != '#Summary: \n':
+        pass
+    f.readline()
+    f.readline()
+    f.readline()
+    values = f.readline().split('|')
+    totrecorded = float(values[-2].strip())
+
+    print("{:.1f} fb^-1".format(totrecorded*1000))
