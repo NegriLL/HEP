0a906bc1e1304390cfb4c45e0fa93fe95400cdb1
Negri Leonardo <nele@cubbli22-gold.local>
2023-02-07 01:18:19 +0200

    create Makefile and edited test program main.cpp

--- Ex3/leonardo/main.cpp
+++ Ex3/leonardo/main.cpp
@@ -5,9 +5,29 @@ using namespace std;
 
 int main()
 {
-
-    twoD myValues(1, 2);
-    cout << myValues.getX() << endl;
+    int x = 1;
+    int y = 2;
+    
+    float sxx = 0.1;
+    float syy = 0.2;
+    float sxy = 0.3;
+    
+    twoD myValues(x, y);
+    cout << "Value X: " <<  myValues.getX() << endl;
+    cout << "Value Y: " <<  myValues.getY() << endl;
+    cout << "Distance: " << myValues.distance() << endl;
+    cout << endl;
+    
+    myValues.setErrorMatrix(sxx, syy, sxy);
+    
+    cout << "Errors: " << myValues.getMatrix(0,0) << " " << myValues.getMatrix(0,1) << endl;
+    cout << "        " << myValues.getMatrix(1,0) << " " << myValues.getMatrix(1,1);
+    
+    cout << endl;
+    
+    cout << "The error is: " << myValues.getError() << endl;
+    cout << "The significance is: " << myValues.getSignificance() << endl;
+    
     return 0;
 
 }
