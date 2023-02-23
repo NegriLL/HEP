e75b0bb2af0c7486dd7a02f7b47fd442cf1017a8
Negri Leonardo <nele@cubbli22-gold.local>
2023-02-23 21:20:21 +0200

    finish exercise 5

--- Ex5/leonardo/makeTree.C
+++ Ex5/leonardo/makeTree.C
@@ -5,15 +5,13 @@ void makeTree() {
   TTree* tree = new TTree("tree", "Random Numbers");
 
   double x;
-
   tree->Branch("x", &x, "x/D");
 
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < 1000; i++){
     x = rand.Gaus(0, 1);
     tree->Fill();
   }
 
-  // Save the tree to a TFile
   TFile* file = new TFile("random_numbers.root", "RECREATE");
   tree->Write();
   file->Close();
